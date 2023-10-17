10260918d2964b2a3f41adfbbee1fb9f3dea5b99
tinkertoe <40361609+tinkertoe@users.noreply.github.com>
2023-10-17 13:59:20 +0200

    -

--- client/src/handler.ts
+++ client/src/handler.ts
@@ -1,9 +1,13 @@
 import loadFrames from './loadFrames'
 import dgram from 'node:dgram'
+import { exec, spawn, execFile } from 'node:child_process'
+import { join } from 'node:path'
 
 export default function(address: string, port: string, directory: string, fps: string) {
   const frames = loadFrames(directory)
   const socket = dgram.createSocket('udp4')
+
+  const audio = exec(`mplayer ${join(directory, 'audio.wav')}`)
 
   let currentFrame = 0
 
